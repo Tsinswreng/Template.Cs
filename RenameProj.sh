5b5c99c5ae53965889316f0d2d3471b85b9b3055
Tsinswreng <Tsinswreng@gmail.com>
2026-06-19 23:15:43 +0800

    改RenameProj.sh

--- RenameProj.sh
+++ RenameProj.sh
@@ -5,4 +5,6 @@
 [ $# -ne 1 ] && echo "用法: $0 <新名称>" && exit 1
 mv ./proj/Cs/Cs.csproj ./proj/Cs/$1.csproj
 mv ./proj/Cs ./proj/$1
+mv ./proj/Cs.Test/Cs.Test.csproj ./proj/Cs.Test/$1.Test.csproj
+mv ./proj/Cs.Test ./proj/$1.Test
 echo "✅ 重命名完成"
